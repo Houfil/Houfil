c7c0582b320177349dc559ad3a05e92cc34ea67f
Houfil <contact@houfil.fr>
2026-07-27 11:42:27 +0200

    Update game URLs in GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
-    [123456789] = "https://raw.githubusercontent.com/TonPseudo/Houfil-Hub/main/Scripts/Tycoon_Houfil_V5.lua", -- War Tycoon
-    [987654321] = "https://raw.githubusercontent.com/TonPseudo/Houfil-Hub/main/Scripts/Autre_Jeu.lua",      -- +1 SpeedKeyboards
+    [123456789] = "https://raw.githubusercontent.com/TonPseudo/Houfil-Hub/main/Scripts/WarTycoon.lua", -- War Tycoon
+    [9584852943] = "https://raw.githubusercontent.com/TonPseudo/Houfil-Hub/main/Scripts/Autre_Jeu.lua",      -- +1 SpeedKeyboards
 }
 
 return Games
