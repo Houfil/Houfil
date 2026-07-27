c53d4ea4cdccb23704ab77d64970a31bf5dd02fe
Houfil <contact@houfil.fr>
2026-07-27 11:25:21 +0200

    Add GameList.lua with game URLs

---
+++ GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+    [123456789] = "https://raw.githubusercontent.com/TonPseudo/Houfil-Hub/main/Scripts/Tycoon_Houfil_V5.lua", -- War Tycoon
+    [987654321] = "https://raw.githubusercontent.com/TonPseudo/Houfil-Hub/main/Scripts/Autre_Jeu.lua",      -- +1 SpeedKeyboards
+}
+
+return Games
