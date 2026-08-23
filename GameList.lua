dc4e3b4ca33df54730fe62fb5f7af751950c3871
Houfil <contact@houfil.fr>
2026-08-24 01:11:55 +0200

    Delete GameList.lua

--- GameList.lua
+++
@@ -1,6 +0,0 @@
-local Games = {
-    [123456789] = "https://raw.githubusercontent.com/TonPseudo/Houfil-Hub/main/Scripts/WarTycoon.lua", -- War Tycoon
-    [9584852943] = "https://raw.githubusercontent.com/TonPseudo/Houfil-Hub/main/Scripts/Autre_Jeu.lua",      -- +1 SpeedKeyboards
-}
-
-return Games
